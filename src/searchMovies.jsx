6f040f6c22f979f613569e2076b82a8d853cca7d
BobbyTarantino76 <matijaazuber@gmail.com>
2026-03-05 20:15:22 +0100

    commit the .env example file for the api

--- src/searchMovies.jsx
+++ src/searchMovies.jsx
@@ -6,12 +6,11 @@ export default function SearchMovies(){
 
         const query = "Jurassic Park"
 
-              const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
 
             const res = await fetch(url);
             const data = await res.json();
             console.log(data)
-
     }
 
     return(
